2cc0038feaf0d63475c2d82b8a964dad17332732
Raziel Navarro <elrazand@gmail.com>
2024-01-25 20:32:01 -0500

    view stats button is now greyish

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import Link from 'next/link';
 import StatsRetrieval from './components/StatsRetrieval';
 import ViewStats from './components/ViewStats';
 import InputText from './components/inputText';
+import background from '../../public/background.png';
+import Image from 'next/image';
 
 export default function Home() {
   const [playerName, setPlayerName] = useState('');
@@ -42,9 +44,11 @@ export default function Home() {
       <header>
         <Navbar />
       </header>
-
-      <main className="flex flex-col min-h-screen justify-center items-center">
-        <h1>Fortnite Stats Tracker</h1>
+      <main className="flex flex-col min-h-screen justify-center items-center mb-1">
+        {/* <div className='absolute -z-10 w-full'>
+        <Image src={background} alt='background image' className='w-full'/>
+        </div> */}
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Fortnite Stats Tracker</h1>
         <div className="input-container flex items-center">
           <InputText placeholder="Enter your EPIC name"
             value={playerName}
@@ -56,8 +60,49 @@ export default function Home() {
         </div>
         {/* Pass the input value and the onFetch function to StatsRetrieval */}
         <StatsRetrieval name={playerName} onFetch={setFetchedData} />
+        <section className='flex justify-center items-center mt-20'>
+        <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Player
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Wins
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Ananda
+                </th>
+                <td class="px-6 py-4">
+                    3210
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Razant
+                </th>
+                <td class="px-6 py-4">
+                    2911
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Antonio
+                </th>
+                <td class="px-6 py-4">
+                    1952
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+      </section>
       </main>
-
       <footer></footer>
     </>
   );
