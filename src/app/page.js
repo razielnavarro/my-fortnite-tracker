32bc6b36b268e2042da3d13964444637038ac647
Raziel Navarro <elrazand@gmail.com>
2024-02-17 23:54:54 -0500

    Replaced form with <Link> and useSearchParams

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,12 @@
 'use client'
-import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import { useRouter } from 'next/navigation';
-
-
+import Link from 'next/link';
+import { useState } from "react"; 
 
 export default function Home() {
-
-  const [statsData, setStatsData] = useState(null);
   const router = useRouter();
-  
-  const fetchPlayers = async (name, accountType, timeWindow, image) => {
-    try {
-      const apiUrl = `https://fortnite-api.com/v2/stats/br/v2?name=${name}&accountType=${accountType}&timeWindow=${timeWindow}&image=${image}`;
-      const response = await fetch(apiUrl, {
-        headers: {
-          'Authorization': '7f63a1da-a518-49b2-84ab-6777a1775b36',
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error.message);
-      throw error;
-    }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const name = formData.get('name');
-    const accountType = 'epic';
-    const timeWindow = 'lifetime';
-    const image = 'all';
-
-    try {
-      const data = await fetchPlayers(name, accountType, timeWindow, image);
-
-      // Check if the data is not empty or undefined
-      if (data && data.data) {
-        setStatsData(data.data);
-
-        // Log the received data to the console
-        console.log('Received Data:', data.data);
-        router.push(`/profile/${encodeURIComponent(name)}`);
-      } else {
-        // Handle the case where the data is not as expected
-        console.error('Invalid data received from the API');
-      }
-    } catch (error) {
-      // Handle error (display a message to the user, etc.)
-    }
-  };
+  const [name, setName] = useState('');
 
   return (
     <>
@@ -65,27 +15,31 @@ export default function Home() {
       </header>
       <main className="flex flex-col min-h-screen justify-center items-center mb-1">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Fortnite Stats Tracker</h1>
-        
-        <form className="flex items-center" onSubmit={handleSubmit}>
-          <label htmlFor="name">
-            <input
-              className="block h-12 w-full rounded-md border border-slate-800 bg-black px-3 py-2 focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-50"
-              placeholder="Enter your EPIC name"
-              name="name"
-              type="text"
-              id="name"
-            />
-          </label>
-          <div className="ml-1">
-            <button
-              className="inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-gradient-to-t from-[#35495e] from-0% to-[#2c3e50] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-              type="submit"
-            >
-              View Stats
-            </button>
-          </div>
-        </form>
 
+      <div className="flex items-center">
+      <label htmlFor="name">
+        <input
+          className="block h-12 w-full rounded-md border border-slate-800 bg-black px-3 py-2 focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-50"
+          placeholder="Enter your EPIC name"
+          name="name"
+          type="text"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      
+      <div className="ml-1">
+        <Link legacyBehavior
+            href={{
+            pathname: `/profile/${name}`,
+            query: {
+            name: name,
+      },
+    }}
+    ><a className='inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-gradient-to-t from-[#35495e] from-0% to-[#2c3e50] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>View Stats</a></Link>
+    </div>
+    </div>
+    
         {/* Section below the input field which shows leaderboards */}
 
         <section className='flex justify-center items-center mt-20'>
--- src/app/profile/[name]/page.js
+++ src/app/profile/[name]/page.js
@@ -1,7 +1,50 @@
-'use client';
+'use client'
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Navbar from '../../components/Navbar';
 
 export default function Profile() {
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+  const [statsData, setStatsData] = useState(null);
+  const apiKey = process.env.NEXT_PUBLIC_FORTNITE_API_KEY;
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      try {
+        const accountType = 'epic';
+        const timeWindow = 'lifetime';
+        const image = 'all';
+
+        const apiUrl = `https://fortnite-api.com/v2/stats/br/v2?name=${name}&accountType=${accountType}&timeWindow=${timeWindow}&image=${image}`;
+
+        const response = await fetch(apiUrl, {
+          headers: {
+            'Authorization': apiKey,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+
+        if (data && data.data) {
+          setStatsData(data.data);
+          console.log('Received Data:', data.data);
+        } else {
+          console.error('Invalid data received from the API');
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    if (name) {
+      fetchPlayers();
+    }
+  }, [name]);
 
   return (
     <>
@@ -9,7 +52,19 @@ export default function Profile() {
         <Navbar />
       </header>
       <main>
-
+        {/* Display the image and other information using statsData */}
+        {/* Check if statsData is not null before accessing properties */}
+        {statsData && (
+          <>
+            <div className='flex justify-center items-center py-9'>
+            <figure>
+              <img className="h-auto max-w-full rounded-lg"src={statsData.image} alt="Stats Image" />
+              <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{name} stats</figcaption>
+              </figure>
+            </div>
+            {/* Display other information from statsData */}
+          </>
+        )}
       </main>
     </>
   );
