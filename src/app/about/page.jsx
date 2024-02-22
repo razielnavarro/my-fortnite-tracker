082260e2ac02a580ddec23e29e369d25c7e2a1c8
Raziel Navarro <elrazand@gmail.com>
2024-02-22 01:05:24 -0500

    Added about page and redirect to it

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import Navbar from '../components/Navbar';
+
+const AboutPage = () => {
+  return (
+    <>
+<header>
+    <Navbar />
+  </header>
+    <div>
+      <h1 className='mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white mt-5'>About me</h1>
+      <p>I am Raziel and I made this as a personal project to showcase my skills :)</p>
+      <h1 className='mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white mt-5'>My Github: </h1>
+        <a href="https://github.com/razielnavarro" target="_blank" rel="noopener noreferrer">https://github.com/razielnavarro</a>
+    </div>
+    </>
+  );
+};
+export default AboutPage;
