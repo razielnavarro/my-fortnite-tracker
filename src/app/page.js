ed6bfa01fe202d0458288a80696e37f405723e68
Raziel Navarro <elrazand@gmail.com>
2024-02-24 17:22:57 -0500

    Added shop API and it works

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,41 @@
 import Navbar from './components/Navbar';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 
 export default function Home() {
   const router = useRouter();
   const [name, setName] = useState('');
+  const [itemShopData, setItemShopData] = useState(null);
+  const apiKey = process.env.NEXT_PUBLIC_FORTNITE_API_KEY_SHOP;
+
+  useEffect(() => {
+    const fetchItemShopData = async () => {
+        try{
+            const apiUrl = "https://fnbr.co/api/shop";
+
+            const response = await fetch(apiUrl, {
+                headers: {
+                    'x-api-key': apiKey
+                },
+            });
+        
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            
+            const data = await response.json();
+
+            setItemShopData(data.data);
+            console.log("Item Shop Data:", data.data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    fetchItemShopData();
+}, []);
+
 
   return (
     <>
@@ -45,7 +75,18 @@ export default function Home() {
 
 <section className='flex flex-col items-center mt-20'>
     <h1 className="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Daily item shop</h1>
-
+    <div className='grid grid-cols-6 gap-4'>
+      {itemShopData && itemShopData.featured && itemShopData.featured.map((item) => (
+        <div key={item.id} className="text-center">
+        <img
+        src={item.images.gallery}
+        alt={item.name}
+        className="h-40 w-40 object-cover mx-auto mb-2"/>
+        <p className="text-white">{item.name}</p>
+        <p className="text-green-500">{`${item.price} ${item.priceIcon}`}</p>
+        </div>
+      ))}
+    </div>
       </section>
       </main>
     </>
