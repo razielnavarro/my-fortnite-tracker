e5006bbc493298d69d6383cd40819f3bb4237942
Raziel Navarro <elrazand@gmail.com>
2024-02-25 23:17:10 -0500

    Removed nextJS config code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      loader: "Razant",
-      path: "/",
-    },
-  };
+const nextConfig = {}
+
 module.exports = nextConfig
