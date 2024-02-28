ef915b6ea94b269da8aa38b9fb285faafd8e6a4a
Raziel Navarro <elrazand@gmail.com>
2024-02-28 00:39:44 -0500

    Added default path again fornext config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        loader: "Razant",
+        path: "/",
+    },
+};
 module.exports = nextConfig
